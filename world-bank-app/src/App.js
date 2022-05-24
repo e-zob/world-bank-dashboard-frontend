92f01a44fc8dc8da1cf1a7e9a9a7454083d1baef
Faye <schufaye@googlemail.com>
2022-05-24 12:46:25 +0100

    edited app.js

--- world-bank-app/src/App.js
+++ world-bank-app/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./Pages/Login";
+import { Login, CreateAccount } from "./Pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />
-        {/* <Route path="create-account" element={<CreateAccount />} /> */}
+        <Route path="/" element={<Login />} />
+        <Route path="/create-account" element={<CreateAccount />} />
       </Routes>
     </Router>
   );
