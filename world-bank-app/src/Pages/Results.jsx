cfa717cbae5a01e0762b8ec96ba5f11804ba4688
Faye <schufaye@googlemail.com>
2022-05-25 17:36:51 +0100

    added functionality to search

---
+++ world-bank-app/src/Pages/Results.jsx
@@ -0,0 +1,7 @@
+import { useLocation } from "react-router-dom";
+
+export default function Results(props) {
+  //   const location = useLocation();
+  //   const results = location.state.results;
+  return <div>Results</div>;
+}
