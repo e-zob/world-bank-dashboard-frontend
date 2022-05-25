2978b094128cff5e8ee76af47d5cb820d32c2019
Faye <schufaye@googlemail.com>
2022-05-25 14:11:40 +0100

    added log user out after logs in test

--- world-bank-app/src/Pages/LoginNetworking.js
+++ world-bank-app/src/Pages/LoginNetworking.js
@@ -14,8 +14,19 @@ export async function postUserInfo(username, password, urlPath) {
   return json;
 }
 
-export async function deleteUserInfo(username, urlPath) {
-  const response = await fetch(`http://localhost:8080/${urlPath}`, {
+export async function logUserOut() {
+  const response = await fetch(`http://localhost:8080/sessions`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const json = response.json();
+  return json;
+}
+
+export async function deleteUserInfo(username) {
+  const response = await fetch(`http://localhost:8080/users`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
--- world-bank-app/tests.js
+++ world-bank-app/tests.js
@@ -1,15 +1,20 @@
 import { assertEquals } from "https://deno.land/std@0.140.0/testing/asserts.ts";
-import { postUserInfo, deleteUserInfo } from "./src/Pages/LoginNetworking.js";
+import {
+  postUserInfo,
+  deleteAccount,
+  logUserOut,
+} from "./src/Pages/LoginNetworking.js";
 
 Deno.test("Creating an account creates an account", async () => {
-  assertEquals(await postUserInfo("xyz", "hello1234", "users"), {
+  assertEquals(await postUserInfo("developmentTest", "hello1234", "users"), {
     response: "Success",
   });
-  await deleteUserInfo("xyz", "users");
+  await deleteAccount("developmentTest");
 });
 
 Deno.test("Logging in logs an existing user in", async () => {
   assertEquals(await postUserInfo("ellie", "123", "sessions"), {
     response: "Success",
   });
+  await logUserOut()
 });
