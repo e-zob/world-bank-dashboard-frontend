43f267548775ee840e253672b2328190c3803c1b
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-05-27 15:48:30 +0100

    fixed fetch bug

--- world-bank-app/src/Networking/ResultsNetworking.js
+++ world-bank-app/src/Networking/ResultsNetworking.js
@@ -1,5 +1,5 @@
 export async function fetchSearchData(urlPath) {
-  const response = await fetch(`http://process.env.REACT_APP_API_URL/${urlPath}`, {
+  const response = await fetch(`http://${process.env.REACT_APP_API_URL}/${urlPath}`, {
     credentials: "include",
   });
   const data = await response.json();
