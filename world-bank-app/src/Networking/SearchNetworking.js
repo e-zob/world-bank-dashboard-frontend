5588987cb710c27d5ee24bf4e22a72eb5c4308ed
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-05-27 02:39:37 +0100

    autoselect for country and indicator

--- world-bank-app/src/Networking/SearchNetworking.js
+++ world-bank-app/src/Networking/SearchNetworking.js
@@ -12,3 +12,9 @@ export async function postSearchData(countries, years, indicator) {
   });
   return response;
 }
+
+export async function getAutocompleteOptions() {
+  const result = await fetch(`http://localhost:8080/autocomplete`);
+  const data = await result.json();
+  return data.response;
+}
