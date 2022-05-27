be596a7d7c284ff7c7c0550f660866275808af96
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-05-27 15:59:27 +0100

    c

--- world-bank-app/src/Networking/SearchNetworking.js
+++ world-bank-app/src/Networking/SearchNetworking.js
@@ -1,5 +1,5 @@
 export async function postSearchData(countries, years, indicator) {
-  const response = await fetch(`http://${process.env.REACT_APP_API_URL}/search`, {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/search`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -15,7 +15,7 @@ export async function postSearchData(countries, years, indicator) {
 }
 
 export async function getAutocompleteOptions() {
-  const result = await fetch(`http://${process.env.REACT_APP_API_URL}/autocomplete`);
+  const result = await fetch(`${process.env.REACT_APP_API_URL}/autocomplete`);
   const data = await result.json();
   return data.response;
 }
